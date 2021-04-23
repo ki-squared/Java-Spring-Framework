8d69d3b1c4584bb9d2f79934f95c9f274d23ae28
abtjade <abt.jade@gmail.com>
2021-04-23 16:50:25 +0900

    Solution 10: JdbcContext

--- Mid-UserDao/src/main/java/kr/ac/jejunu/userdao/StatementStrategy.java
+++ Mid-UserDao/src/main/java/kr/ac/jejunu/userdao/StatementStrategy.java
@@ -5,5 +5,5 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public interface StatementStrategy {
-    abstract PreparedStatement makeStatement(Object object, Connection connection) throws SQLException;
+    abstract PreparedStatement makeStatement(Connection connection) throws SQLException;
 }
